176a708660b21cdc06c7a545e9a512f138f6fd14
Leonan Reis <146787577+LeonanAraudo@users.noreply.github.com>
2025-05-05 23:21:08 -0300

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -5,7 +5,14 @@ dotenv.config()
 const envSchema = z.object({
   DATABASE_URL: z.string().url(),
 })
-export const env = envSchema.parse(process.env)
+
+try {
+  export const env = envSchema.parse(process.env)
+} catch (error) {
+  console.error("Erro de validação de ambiente:", error.errors)
+  process.exit(1)
+}
+
 
 //process.env e a variavel de ambiente que ta no .env
 //quando executa o parse, ele vai verificar se o process.env vai seguir o formato de envSchema
